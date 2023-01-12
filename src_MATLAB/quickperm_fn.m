e04de344e1a5631e1ea93c8cade5667120f5a195
Ty Easley <tyoeasley@gmail.com>
2023-01-11 23:52:54 -0600

    Removed subject-specific PROFUMO data

--- src_MATLAB/quickperm_fn.m
+++ src_MATLAB/quickperm_fn.m
@@ -1,5 +1,6 @@
 % computes a list of admissible permutations from a file defining a set of Exchnageability Blocks [1]
 % [1] Winkler et al. (2015) https://doi.org/10.1016/j.neuroimage.2015.05.092
+% requires Matlab library PALM
 
 function [] = quickperm_fn(EB_fileloc,n_perm,floc_out)
 
