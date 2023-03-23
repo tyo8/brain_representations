be695ee2c84edd77e0601d8836b7a1d1ff02168b
root <tyoeasley@gmail.com>
2023-03-23 14:08:04 -0500

    removed top-layer cluster submission scripts

--- pull_from_cluster.sh
+++ pull_from_cluster.sh
@@ -3,4 +3,4 @@
 src_data_spec="tyoeasley@login3.chpc.wustl.edu:/scratch/tyoeasley/brain_representations"
 target_dir="/home/tyo/Documents/Personomics_Lab/Experiments/brain_representations"
 
-rsync -rv ${src_data_spec}/* ${target_dir} --max-size=26K --exclude=*phom_data* --exclude=*.txt --exclude=*.nii.gz* --exclude=logs --exclude=*sub-* --exclude=*subj-*.csv* --exclude=*.html --exclude=*.hdf5 --exclude=*.m --exclude=*MATLAB* --exclude=*dist_mtx* --exclude=*data.csv --exclude=*.pfm --exclude=HCP_PTN1200 --exclude=*deprecated*
+rsync -rv ${src_data_spec}/* ${target_dir} --max-size=26K --exclude=*phom_data* --exclude=*.txt --exclude=*.nii.gz* --exclude=logs --exclude=*sub-* --exclude=*subj-*.csv* --exclude=*.html --exclude=*.hdf5 --exclude=*.m --exclude=*MATLAB* --exclude=*dist_mtx* --exclude=*data.csv --exclude=*.pfm --exclude=HCP_PTN1200 --exclude=*deprecated* --exclude=*RESTRICTED*
