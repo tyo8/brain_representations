0b0ca88c3a6e5bddbb6695eed0c623c828749bf5
Ty Easley <tyoeasley@gmail.com>
2024-12-12 12:16:24 -0600

    remove outdated

--- import_profumo_reps.sh
+++
@@ -1,1 +0,0 @@
-rsync -rv tyo8teasley@login.chpc.wustl.edu:/scratch/janine/HCP_PFM/HCP_Profumo_cifti_bigdata_1200subs_50modes_smooth_5mm_4runs.pfm /scratch/tyoeasley/brain_representations/profumo_reps
